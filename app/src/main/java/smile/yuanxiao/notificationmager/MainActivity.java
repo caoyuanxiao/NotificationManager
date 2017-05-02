a0981dbc82d9806e6f8f9725e62ec8e60dd24507
kc <344103018@qq.com>
2017-05-02 18:27:48 +0800

    本地

--- app/src/main/java/smile/yuanxiao/notificationmager/MainActivity.java
+++ app/src/main/java/smile/yuanxiao/notificationmager/MainActivity.java
@@ -1,13 +1,39 @@
 package smile.yuanxiao.notificationmager;
 
+import android.app.Notification;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RemoteViews;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final int TYPE_NOMAL = 1;
+    public static final int TYPE_DOWNLOAD = 2;
+    public static final int BIGTEXTSTYLE = 3;
+    public static final int INDEX_BOX = 4;
+    public static final int BIG_BITMAP = 5;
+    public static final int HEADED_UP=6;
+
+
     android.app.NotificationManager manager;
+
+
+    public Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what < 100) {
+                mRemoteViews.setTextViewText(R.id.tv_progress, msg.what + "%");
+                mRemoteViews.setProgressBar(R.id.notifi_progressbar, 100, msg.what, false);
+                manager.notify(TYPE_DOWNLOAD, notification);
+            } else {
+                mRemoteViews.setTextViewText(R.id.tv_progress, "下载完成");
+            }
+        }
+    };
 
     /*http://blog.csdn.net/w804518214/article/details/51231946*/
     @Override
@@ -17,7 +43,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         manager = (android.app.NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         findViewById(R.id.notification_nomal).setOnClickListener(this);
+        findViewById(R.id.notification_download).setOnClickListener(this);
+        findViewById(R.id.notification_bigtextstyle).setOnClickListener(this);
+        findViewById(R.id.notification_IndexBox).setOnClickListener(this);
+        findViewById(R.id.notification_BigBitmap).setOnClickListener(this);
+        findViewById(R.id.notification_FullScreen).setOnClickListener(this);
     }
+
+    Notification notification;
+    RemoteViews mRemoteViews;
 
     @Override
     public void onClick(View v) {
@@ -25,7 +59,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.notification_nomal:
                 NotificationManager.NOTIFICATION_MANAGER.NomalNotifi(this, manager, TYPE_NOMAL);
                 break;
+            case R.id.notification_download:
+                notification = NotificationManager.NOTIFICATION_MANAGER.DownloadNotification(this, manager, TYPE_NOMAL);
+                mRemoteViews = notification.contentView;
+
+                for (int i = 1; i <= 100; i++) {
+                    final int finalI = i;
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                Thread.sleep(200);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            mHandler.sendEmptyMessage(finalI);
+                        }
+                    }).start();
+
+                }
+
+                break;
+            case R.id.notification_bigtextstyle:
+                NotificationManager.NOTIFICATION_MANAGER.BigTextStyleNotification(this, manager);
+                break;
+            case R.id.notification_IndexBox:
+                NotificationManager.NOTIFICATION_MANAGER.inBoxStyle(this, manager);
+                break;
+            case R.id.notification_BigBitmap:
+                NotificationManager.NOTIFICATION_MANAGER.bigPictureStyle(this, manager);
+                break;
+            case R.id.notification_FullScreen:
+                break;
         }
-        ;
+
     }
 }
--- app/src/main/java/smile/yuanxiao/notificationmager/NotificationManager.java
+++ app/src/main/java/smile/yuanxiao/notificationmager/NotificationManager.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.NotificationCompat;
+import android.widget.RemoteViews;
 
 /**
  * Created by Smile on 2017/5/2.
@@ -17,7 +18,7 @@ public enum NotificationManager {
     public void NomalNotifi(Context mContext, android.app.NotificationManager manager, int type) {
         //为了版本兼容  选择V7包下的NotificationCompat进行构造
         NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
-        //Ticker是状态栏显示的提示
+        //Ticker是状态栏显示的提示  这里的提示仅仅是针对android5.0以下才可以显示
         builder.setTicker("简单Notification");
         //第一行内容  通常作为通知栏标题
         builder.setContentTitle("标题");
@@ -34,7 +35,7 @@ public enum NotificationManager {
         //系统状态栏显示的小图标
         builder.setSmallIcon(R.mipmap.ic_launcher);
         //下拉显示的大图标
-        builder.setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(), R.drawable.ic_stat_name));
+        builder.setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.ic_launcher_round));
         PendingIntent pengdingIntent = createPengdingIntent(mContext);
         //点击跳转的intent
         builder.setContentIntent(pengdingIntent);
@@ -42,6 +43,146 @@ public enum NotificationManager {
         builder.setDefaults(NotificationCompat.DEFAULT_ALL);
         Notification notification = builder.build();
         manager.notify(type, notification);
+    }
+
+
+    public Notification DownloadNotification(Context mContext, android.app.NotificationManager manager, int type) {
+        int progress = 10;
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.ic_launcher_round));
+        //禁止用户点击删除按钮删除
+        builder.setAutoCancel(false);
+        //禁止滑动删除
+        // builder.setOngoing(true);
+        //取消右上角的时间显示
+        // builder.setShowWhen(false);
+        PendingIntent pengdingIntent = createPengdingIntent(mContext);
+        builder.setContentIntent(pengdingIntent);
+
+        RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(), R.layout.download_progress);
+        // builder.setCustomContentView(remoteViews);
+//        builder.setContentTitle("下载中..."+progress+"%");
+//        builder.setProgress(100,progress,false);
+        //builder.setContentInfo(progress+"%");
+
+//        Intent intent = new Intent(this,DownloadService.class);
+//        intent.putExtra("command",1);
+        Notification notification = builder.build();
+        notification.contentView = remoteViews;
+        // manager.notify(type,notification);
+        return notification;
+
+    }
+
+    /**
+     * 点击后展开可显示大段文字内容的通知
+     * 只有在首界面才会全部显示
+     * 1. 使用类 Android.support.v4.app.NotificationCompat.BigTextStyle
+     2. 在低版本系统上只显示点击前的普通通知样式 如4.4可以点击展开，在4.0系统上就不行
+     3. 点击前后的ContentTitle、ContentText可以不一致，bigText内容可以自动换行 好像最多5行的样子
+     *
+     * @param mContext
+     * @param manager
+     */
+    public void BigTextStyleNotification(Context mContext, android.app.NotificationManager manager) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
+        builder.setContentTitle("BigTextStyle");
+        builder.setContentText("BigTextStyle演示示例");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.ic_launcher_round));
+        android.support.v4.app.NotificationCompat.BigTextStyle style = new android.support.v4.app.NotificationCompat.BigTextStyle();
+        style.bigText("这里是点击通知后要显示的正文，可以换行可以显示很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长这里是点击通知后要显示的正文，可以换行可以显示很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长这里是点击通知后要显示的正文，可以换行可以显示很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长很长");
+        style.setBigContentTitle("点击后的标题");
+        //SummaryText没什么用 可以不设置
+        style.setSummaryText("末尾只一行的文字内容");
+        builder.setStyle(style);
+        builder.setAutoCancel(true);
+
+        builder.setContentIntent(createPengdingIntent(mContext));
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        Notification notification = builder.build();
+        manager.notify(MainActivity.BIGTEXTSTYLE, notification);
+    }
+
+    /**
+     * 只有在首界面才会全部显示
+     * 注意事项
+     1. 使用类android.support.v4.app.NotificationCompat.InboxStyle
+     2. 每行内容过长时并不会自动换行
+     3. addline可以添加多行 但是多余5行的时候每行高度会有截断
+     4. 同BigTextStyle 低版本上系统只能显示普通样式
+     *
+     * @param mContext
+     * @param manager
+     */
+    public void inBoxStyle(Context mContext, android.app.NotificationManager manager) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
+        builder.setContentTitle("InboxStyle");
+        builder.setContentText("InboxStyle演示示例");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.ic_launcher_round));
+        android.support.v4.app.NotificationCompat.InboxStyle style = new android.support.v4.app.NotificationCompat.InboxStyle();
+        style.setBigContentTitle("BigContentTitle")
+                .addLine("第一行，第一行，第一行，第一行，第一行，第一行，第一行")
+                .addLine("第二行")
+                .addLine("第三行")
+                .addLine("第四行")
+                .addLine("第五行")
+                .addLine("第五行")
+                .addLine("第五行")
+                .addLine("第五行")
+                .addLine("第五行")
+                .setSummaryText("SummaryText");
+        builder.setStyle(style);
+        builder.setAutoCancel(true);
+
+        builder.setContentIntent(createPengdingIntent(mContext));
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        Notification notification = builder.build();
+        manager.notify(MainActivity.INDEX_BOX, notification);
+    }
+
+    /**
+     * 1. 使用类android.support.v4.app.NotificationCompat.BigPictureStyle
+     2. style.bigPicture传递的是个bitmap对象 所以也不应该传过大的图 否则会oom
+     3. 同BigTextStyle 低版本上系统只能显示普通样式
+     * @param mContext
+     * @param manager
+     */
+    public void bigPictureStyle(Context mContext, android.app.NotificationManager manager){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
+        builder.setContentTitle("BigPictureStyle");
+        builder.setContentText("BigPicture演示示例");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        builder.setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(),R.mipmap.ic_launcher_round));
+        android.support.v4.app.NotificationCompat.BigPictureStyle style = new android.support.v4.app.NotificationCompat.BigPictureStyle();
+        style.setBigContentTitle("BigContentTitle");
+        style.setSummaryText("SummaryText");
+        style.bigPicture(BitmapFactory.decodeResource(mContext.getResources(),R.mipmap.liushishi));
+        builder.setStyle(style);
+        builder.setAutoCancel(true);
+        //设置点击大图后跳转
+        builder.setContentIntent(createPengdingIntent(mContext));
+        Notification notification = builder.build();
+        manager.notify(MainActivity.BIG_BITMAP,notification);
+    }
+
+    public void FullScreenNotification(Context mContext, android.app.NotificationManager manager){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
+        builder.setContentTitle("横幅通知");
+        builder.setContentText("请在设置通知管理中开启消息横幅提醒权限");
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(),R.mipmap.ic_launcher_round));
+
+        builder.setContentIntent(createPengdingIntent(mContext));
+        //这句是重点
+        builder.setFullScreenIntent(createPengdingIntent(mContext),true);
+        builder.setAutoCancel(true);
+        Notification notification = builder.build();
+        manager.notify(MainActivity.HEADED_UP,notification);
     }
 
     public PendingIntent createPengdingIntent(Context mContext) {
